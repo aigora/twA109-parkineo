a7bba8d206e63bc6c1c063358bd08faefd360d48
mlotruglio <m.lotruglio@alumnos.upm.es>
2019-05-12 02:14:39 +0200

    Update SerialPort.c

--- SerialPort.c
+++ SerialPort.c
@@ -1,138 +1,46 @@
-#include "SerialPort.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "SerialPort.h"
 #define MAX_DATA_LENGTH 255
-
+// Funciones prototipo
 void autoConnect(SerialPort *arduino,char*);
-
-void Crear_Conexion(SerialPort *PuertoSerie, char *portName)
+int main(void)
 {
-
-	PuertoSerie->connected = 0;
-	PuertoSerie->portName = portName;
-    PuertoSerie->handler = CreateFileA((portName),
-                                GENERIC_READ | GENERIC_WRITE,
-                                0,
-                                NULL,
-                                OPEN_EXISTING,
-                                FILE_ATTRIBUTE_NORMAL,
-                                NULL);
-    if (PuertoSerie->handler == INVALID_HANDLE_VALUE)
-	{
-        if (GetLastError() == ERROR_FILE_NOT_FOUND)
-		{
-            printf("ERROR: Handle was not attached. Reason: %s not available\n", portName);
-        }
-    	else
-        {
-            printf("ERROR!!!");
-        }
-    }
-    else 
-	{
-        DCB dcbSerialParameters = {0};
-
-        if (!GetCommState(PuertoSerie->handler, &dcbSerialParameters))
-		{
-            printf("failed to get current serial parameters");
-        }
-        else 
-		{
-            dcbSerialParameters.BaudRate = CBR_9600;
-            dcbSerialParameters.ByteSize = 8;
-            dcbSerialParameters.StopBits = ONESTOPBIT;
-            dcbSerialParameters.Parity = NOPARITY;
-            dcbSerialParameters.fDtrControl = DTR_CONTROL_ENABLE;
-
-            if (!SetCommState(PuertoSerie->handler, &dcbSerialParameters))
-            {
-                printf("ALERT: could not set Serial port parameters\n");
-            }
-            else 
-			{
-                PuertoSerie->connected = 1;
-                PurgeComm(PuertoSerie->handler, PURGE_RXCLEAR | PURGE_TXCLEAR);
-                Sleep(ARDUINO_WAIT_TIME);
-            }
-        }
-    }
-    return ;
+//Arduino SerialPort object
+SerialPort *arduino;
+// Puerto serie en el que está Arduino
+char* portName = "\\\\.\\COM5";
+// Buffer para datos procedentes de Arduino
+char incomingData[MAX_DATA_LENGTH];
+// Crear estructura de datos del puerto serie
+arduino = (SerialPort *)malloc(sizeof(SerialPort));
+// Apertura del puerto serie
+Crear_Conexion(arduino,portName);
+autoConnect(arduino,incomingData);
+return 0;
 }
-
 void autoConnect(SerialPort *arduino,char *incomingData)
 {
-	char sendData = 0;
-	// Espera la conexión con Arduino
-	while (!isConnected(arduino))
-	{
-	Sleep(100);
-	Crear_Conexion(arduino,arduino->portName);
-	}
-	//Comprueba si arduino está connectado
-	if (isConnected(arduino))
-	{
-	printf ("Conectado con Arduino en el puerto %s\n",arduino->portName);
-	}
-	// Bucle de la aplicación (Poner nuestro buble del proyecto)
-	printf ("0 - LED OFF, 1 - LED ON, 9 - SALIR");
-	while (isConnected(arduino) && sendData!='9')
-	{
-	sendData = getch();
-	writeSerialPort(arduino,&sendData, sizeof(char));
-	}
-	if (!isConnected(arduino))
-	printf ("Se ha perdido la conexión con Arduino\n");
-}
-
-void CerrarConexion(SerialPort * PuertoSerie)
+char sendData = 0;
+// Espera la conexión con Arduino
+while (!isConnected(arduino))
 {
-    if (PuertoSerie->connected)
-	{
-        PuertoSerie->connected = 0;
-        CloseHandle(PuertoSerie->handler);
-    }
+Sleep(100);
+Crear_Conexion(arduino,arduino->portName);
 }
-
-int readSerialPort(SerialPort * PuertoSerie,char *buffer, unsigned int buf_size)
+//Comprueba si arduino está connectado
+if (isConnected(arduino))
 {
-    DWORD bytesRead;
-    unsigned int toRead = 0;
-
-    ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status);
-
-    if (PuertoSerie->status.cbInQue > 0)
-	{
-        if (PuertoSerie->status.cbInQue > buf_size)
-		{
-            toRead = buf_size;
-        }
-        else toRead = PuertoSerie->status.cbInQue;
-    }
-
-    memset(buffer, 0, buf_size);
-
-    if (ReadFile(PuertoSerie->handler, buffer, toRead, &bytesRead, NULL)) return bytesRead;
-
-    return 0;
+printf ("Conectado con Arduino en el puerto %s\n",arduino->portName);
 }
-
-int writeSerialPort(SerialPort *PuertoSerie,char *buffer, unsigned int buf_size)
+// Bucle de la aplicación
+printf ("0 - LED OFF, 1 - LED ON, 9 - SALIR");
+while (isConnected(arduino) && sendData!='9')
 {
-    DWORD bytesSend;
-
-    if (!WriteFile(PuertoSerie->handler, (void*) buffer, buf_size, &bytesSend, 0))
-	{
-        ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status);
-        return 0;
-    }
-    else return 1;
+sendData = getch();
+writeSerialPort(arduino,&sendData, sizeof(char));
 }
-
-int isConnected(SerialPort *PuertoSerie)
-{
-    if (!ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status))
-		PuertoSerie->connected = 0;
-    return PuertoSerie->connected;
+if (!isConnected(arduino))
+printf ("Se ha perdido la conexión con Arduino\n");
 }
